feef8eb67e98bb864547d9e88e5a390d306ee818
liuguangchao3 <liu3321261@sina.com>
2017-11-04 16:04:06 +0800

    增加上传充电进度

---
+++ piles-record/src/main/java/com/piles/record/business/impl/UploadChargeRateBusinessImpl.java
@@ -0,0 +1,32 @@
+package com.piles.record.business.impl;
+
+
+import com.piles.common.business.BaseBusiness;
+import com.piles.record.entity.UploadChargeMonitorRequest;
+import com.piles.record.entity.UploadChargeRateRequest;
+import com.piles.record.entity.UploadRecordRequest;
+import com.piles.record.service.IUploadChargeRateService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 上传充电进度接口实现
+ */
+@Service("uploadChargeRateBusiness")
+public class UploadChargeRateBusinessImpl extends BaseBusiness {
+
+    @Resource
+    private IUploadChargeRateService uploadChargeRateService;
+
+
+    @Override
+    protected byte[] processBody(byte[] bodyBytes) {
+        //依照报文体规则解析报文
+        UploadChargeRateRequest uploadChargeRateRequest = UploadChargeRateRequest.packEntity(bodyBytes);
+        //调用底层接口
+        uploadChargeRateService.uploadChargeRate(uploadChargeRateRequest);
+        //组装返回报文体
+        return null;
+    }
+}
