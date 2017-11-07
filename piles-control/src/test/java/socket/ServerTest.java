ad26d55ebfba1d8b72589eb3c25255014d5b2741
lgc48027 <lgc48027@ly.com>
2017-11-07 19:17:55 +0800

    push 消息 调试通过 远程启动充电

--- piles-control/src/test/java/socket/ServerTest.java
+++ piles-control/src/test/java/socket/ServerTest.java
@@ -3,11 +3,15 @@ package socket;
 import com.alibaba.fastjson.JSON;
 import com.piles.common.util.ChannelMap;
 import com.piles.control.entity.RemoteClosePushRequest;
+import com.piles.control.entity.RemoteStartPushRequest;
 import com.piles.control.service.IRemoteClosePushService;
+import com.piles.control.service.IRemoteStartPushService;
 import io.netty.channel.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.math.BigDecimal;
 
 public class ServerTest {
     private static final String SRPING_ROOT_XML = "classpath:application.xml";
@@ -26,13 +30,24 @@ public class ServerTest {
             while (true){
                 Channel channel=ChannelMap.getChannel("1000025484561835");
                 if (null!=channel){
-                    IRemoteClosePushService service= context.getBean( IRemoteClosePushService.class );
-                    RemoteClosePushRequest remoteClosePushRequest= new RemoteClosePushRequest();
+//                    IRemoteClosePushService service= context.getBean( IRemoteClosePushService.class );
+//                    RemoteClosePushRequest remoteClosePushRequest= new RemoteClosePushRequest();
+//                    remoteClosePushRequest.setGunNo( 1 );
+//                    remoteClosePushRequest.setOrderNo( 123456L );
+//                    remoteClosePushRequest.setPileNo( "1000025484561835" );
+//                    remoteClosePushRequest.setSerial( "0000" );
+//                    System.out.println("远程结束充电返回信息"+ JSON.toJSONString(  service.doPush( remoteClosePushRequest)));
+
+                    IRemoteStartPushService service= context.getBean( IRemoteStartPushService.class );
+                    RemoteStartPushRequest remoteClosePushRequest= new RemoteStartPushRequest();
                     remoteClosePushRequest.setGunNo( 1 );
                     remoteClosePushRequest.setOrderNo( 123456L );
                     remoteClosePushRequest.setPileNo( "1000025484561835" );
                     remoteClosePushRequest.setSerial( "0000" );
-                    System.out.println("远程结束充电返回信息"+ JSON.toJSONString(  service.doPush( remoteClosePushRequest)));
+                    remoteClosePushRequest.setChargeData( new BigDecimal( 1 ) );
+                    remoteClosePushRequest.setChargeModel( 1 );
+                    remoteClosePushRequest.setChargeStopCode( "646444" );
+                    System.out.println("远程启动充电返回信息"+ JSON.toJSONString(  service.doPush( remoteClosePushRequest)));
 
                 }
                 Thread.sleep(10000L);
