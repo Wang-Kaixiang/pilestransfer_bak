c108964a5453f6aa555206d71803aa21ed3b0999
liuguangchao3 <liu3321261@sina.com>
2018-01-08 23:22:16 +0800

    修改seral

--- piles-setting/src/main/java/com/piles/setting/service/imp/XunDaoGetPileVersionPushServiceImpl.java
+++ piles-setting/src/main/java/com/piles/setting/service/imp/XunDaoGetPileVersionPushServiceImpl.java
@@ -21,7 +21,7 @@ import java.util.concurrent.TimeUnit;
  * 远程升级 给充电桩发送消息实现类
  */
 @Slf4j
-@Service
+@Service("xunDaoGetPileVersionPushServiceImpl_2")
 public class XunDaoGetPileVersionPushServiceImpl implements IGetPileVersionPushService {
 
     @Resource(name = "xunDaoPushBusinessImpl")
