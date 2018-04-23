c7954eba967e5c65377fec11efd6ab72e7758cb0
刘广超 <lgc48027@ly.com>
2018-04-23 11:01:42 +0800

    增加新的功能

--- piles-setting/src/main/java/com/piles/setting/business/impl/XunDaoModifyIPReportBusinessImpl.java
+++ piles-setting/src/main/java/com/piles/setting/business/impl/XunDaoModifyIPReportBusinessImpl.java
@@ -7,15 +7,11 @@ import com.piles.common.business.IBusiness;
 import com.piles.common.entity.type.TradeType;
 import com.piles.common.util.BytesUtil;
 import com.piles.common.util.CRC16Util;
-import com.piles.common.util.ChannelResponseCallBackMap;
 import com.piles.setting.domain.UploadChageIpRecord;
-import com.piles.setting.entity.XunDaoModifyIPPushRequest;
 import com.piles.setting.entity.XunDaoModifyIPReportRequest;
-import com.piles.setting.entity.XunDaoModifyIPRequest;
 import com.piles.setting.service.IUploadChangeIpService;
 import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -33,35 +29,37 @@ public class XunDaoModifyIPReportBusinessImpl implements IBusiness {
     @Override
     public byte[] process(byte[] msg, Channel incoming) {
         log.info( "接收到循道充电桩修改ip上报返回报文" );
-        byte[] dataBytes = BytesUtil.copyBytes(msg, 13, 8);
+        byte[] dataBytes = BytesUtil.copyBytes( msg, 13, 40 );
         //依照报文体规则解析报文
         XunDaoModifyIPReportRequest xunDaoModifyIPRequest = XunDaoModifyIPReportRequest.packEntity( dataBytes );
-        UploadChageIpRecord uploadChageIpRecord=JSONObject.parseObject( JSON.toJSONString( xunDaoModifyIPRequest ), UploadChageIpRecord.class );
+        UploadChageIpRecord uploadChageIpRecord = JSONObject.parseObject( JSON.toJSONString( xunDaoModifyIPRequest ), UploadChageIpRecord.class );
         uploadChageIpRecord.setTradeTypeCode( TradeType.XUN_DAO.getCode() );
-        boolean flag=ipService.uploadRecord(  uploadChageIpRecord);
+        boolean flag = ipService.uploadRecord( uploadChageIpRecord );
 
-        if ( flag){
-            dataBytes=Bytes.concat(dataBytes, new byte[]{0x00});
-        }else {
-            dataBytes=Bytes.concat(dataBytes, new byte[]{0x01});
+        if (flag) {
+            dataBytes = Bytes.concat( dataBytes, new byte[]{0x00} );
+        } else {
+            dataBytes = Bytes.concat( dataBytes, new byte[]{0x01} );
         }
-        byte[] head=buildHead(dataBytes,BytesUtil.copyBytes(msg, 2, 4));
+        byte[] head = buildHead( dataBytes, BytesUtil.copyBytes( msg, 2, 4 ) );
         return head;
     }
 
     //添加报文头
     private byte[] buildHead(byte[] dataMsg, byte[] serial) {
-        byte[] result = Bytes.concat(new byte[]{0x68,0x14});
-        result = Bytes.concat(result, serial);
+        byte[] result = Bytes.concat( new byte[]{0x68, 0x14} );
+        result = Bytes.concat( result, serial );
         //添加类型标识
-        result = Bytes.concat(result, new byte[]{(byte)0x85,0x00,0x03,0x00});
+        result = Bytes.concat( result, new byte[]{(byte) 0x85, 0x00, 0x03, 0x00} );
         //添加crc
-        result = Bytes.concat(result, CRC16Util.getXunDaoCRC(dataMsg));
+        result = Bytes.concat( result, CRC16Util.getXunDaoCRC( dataMsg ) );
         //添加记录类型
-        result = Bytes.concat(result, new byte[]{0x48});
+        result = Bytes.concat( result, new byte[]{0x48} );
 
-        result = Bytes.concat(result, dataMsg);
+        result = Bytes.concat( result, dataMsg );
 
         return result;
     }
+
+
 }
